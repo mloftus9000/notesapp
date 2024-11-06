ccfd48d31d0988647909d20341a6687d810b9932
Matthew Loftus <mloftus@flexport.com>
2024-11-06 14:49:45 -0800

    fix auth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,20 +1,11 @@
-import {type ClientSchema, a, defineData} from "@aws-amplify/backend";
+import {defineAuth} from "@aws-amplify/backend";
 
-const schema = a.schema({
-  Note: a
-    .model({
-      name: a.string(),
-      description: a.string(),
-      image: a.string(),
-    })
-    .authorization((allow) => [allow.owner()]),
-});
-
-export type Schema = ClientSchema<typeof schema>;
-
-export const data = defineData({
-  schema,
-  authorizationModes: {
-    defaultAuthorizationMode: "userPool",
+/**
+ * Define and configure your auth resource
+ * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+ */
+export const auth = defineAuth({
+  loginWith: {
+    email: true,
   },
 });
